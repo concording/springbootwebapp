72b4d3129f999b9d435d0bfba8d6cbf5a63c1093
wangyinbin <demureboylove@gmail.com>
2020-10-27 18:44:26 +0800

    contenttype

--- src/main/java/guru/springframework/controllers/CaocaoController.java
+++ src/main/java/guru/springframework/controllers/CaocaoController.java
@@ -1,14 +1,20 @@
 package guru.springframework.controllers;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CaocaoController {
-    @PostMapping("/order")
+
+
+
+    @RequestMapping(value = "/order", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+            produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     String index(@RequestBody Object object){
 
         Gson gson = new Gson();
