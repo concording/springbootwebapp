c27dbc952312014d612e4df206a25550fe787785
wangyinbin <demureboylove@gmail.com>
2020-10-28 17:21:45 +0800

    更新请求头

--- src/main/java/guru/springframework/controllers/CaocaoController.java
+++ src/main/java/guru/springframework/controllers/CaocaoController.java
@@ -5,10 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 public class CaocaoController {
@@ -19,7 +21,7 @@ public class CaocaoController {
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
             produces = {MediaType.APPLICATION_ATOM_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     @ResponseBody
-    String index( MultiValueMap paramMap) throws Exception{
+    String index(@RequestParam Map<String, String> paramMap) throws Exception{
 
         Gson gson = new Gson();
         logger.info("订单接到回调请求=" + gson.toJson(paramMap));
