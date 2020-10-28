90ab0b145bc547fe246f597af5f1bf3fef320247
wangyinbin <demureboylove@gmail.com>
2020-10-28 16:38:04 +0800

    add log

--- src/main/java/guru/springframework/SpringBootWebApplication.java
+++ src/main/java/guru/springframework/SpringBootWebApplication.java
@@ -2,8 +2,10 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@EnableConfigurationProperties
+@SpringBootApplication(scanBasePackages = "guru.springframework.*")
 public class SpringBootWebApplication {
 
     public static void main(String[] args) {
--- src/main/java/guru/springframework/controllers/CaocaoController.java
+++ src/main/java/guru/springframework/controllers/CaocaoController.java
@@ -1,6 +1,8 @@
 package guru.springframework.controllers;
 
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CaocaoController {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(CaocaoController.class);
 
     @RequestMapping(value = "/order", method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
@@ -18,7 +20,7 @@ public class CaocaoController {
     String index(@RequestBody Object object){
 
         Gson gson = new Gson();
-        System.out.println(gson.toJson(object));
+        logger.info("订单接到回调请求=" + gson.toJson(object));
         return "index";
     }
 }
